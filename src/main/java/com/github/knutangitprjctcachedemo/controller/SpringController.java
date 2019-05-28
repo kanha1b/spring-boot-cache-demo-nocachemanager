109e951f330606f1ab56ab65f5e8835d6c43eba8
kanha1b <kbagherwal@yahoo.com>
2019-05-29 00:27:21 +0530

    cacheevict is added

--- src/main/java/com/github/knutangitprjctcachedemo/controller/SpringController.java
+++ src/main/java/com/github/knutangitprjctcachedemo/controller/SpringController.java
@@ -5,10 +5,12 @@ import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +29,21 @@ public class SpringController {
 		Instant instant = timestamp.toInstant();
 		
 		String s ="welcome to spring cache example by kanha"+instant.toEpochMilli();
+		
+		return s;
+	}
+	
+	@RequestMapping(value="/cacheevict/token/{token_no}", method=RequestMethod.PUT)
+	@CacheEvict(value = "sample", allEntries=true)
+	public String cacheingUpdateMethod(@PathVariable("token_no")int token) throws InterruptedException {
+		
+		
+		Thread.sleep(3000);
+		System.out.println("it is for caching with token number :"+token);
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		Instant instant = timestamp.toInstant();
+		
+		String s ="updated"+instant.toEpochMilli();
 		
 		return s;
 	}
